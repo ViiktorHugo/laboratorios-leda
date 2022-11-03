b9f8e70add27b7d90790e7a8feb7119f6db86553
ViiktorHugo <victor.rangel@ccc.ufcg.edu.br>
2022-11-03 10:01:45 -0300

    r03 changes, counting sort done, needs testing

--- R03-02/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02/src/main/java/sorting/linearSorting/CountingSort.java
@@ -17,8 +17,45 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
+		//tratamento para testes
+		if (array.length == 0 | leftIndex == 0 | rightIndex == 0) {
+			return;
+		}
+
+		//variáveis
+		int menor = array[0];
+		int maior = array[1];
+
+		//encontrando o maior e o menor
+		for (int i=leftIndex; i<rightIndex; i ++) {	
+			if (array[i] < menor) {
+				menor = i;
+			}
+			if (array[i] > maior) {
+				maior = i;
+			}
+		}
+		
+		//registrando frequencia dos elementos do array;
+		int[] arrayContagem = new int [maior - menor];
+		
+		for (int i=leftIndex; i<rightIndex; i++){
+			arrayContagem[array[i]-1] += 1;
+		}
+
+		//calculando a soma cumulativa do array auxiliar
+		for (int i=leftIndex; i<rightIndex; i++) {
+			arrayContagem[i] += arrayContagem[i-1];
+		}
+
+		//ordenando o array
+		int[] arrayFinal = new int [maior - menor];
+
+		for (int i = leftIndex; i >= rightIndex; i++) {
+			arrayFinal[arrayContagem[array[i] - 1] -1] = array[i];
+			arrayContagem[array[i] - 1] -= 1;
+		}
 	}
 
 }
