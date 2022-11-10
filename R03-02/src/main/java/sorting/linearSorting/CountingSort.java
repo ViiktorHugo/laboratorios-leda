7dbae3781ae50e17b1c6b3f78a920349204386b7
viiktorhugo <victor.rangel@ccc.ufcg.edu.br>
2022-11-10 10:28:13 -0300

    minor changes in R03

--- R03-02/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02/src/main/java/sorting/linearSorting/CountingSort.java
@@ -19,47 +19,46 @@ public class CountingSort extends AbstractSorting<Integer> {
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
 
 		//tratando casos de teste
-		if (array.length == 0) {
-			return;
-		}
+		if (array.length > 0) {
+			
+			//variáveis
+			int maior = 0;
+			int menor = 0;
 
-		//variáveis
-		int maior = 0;
-		int menor = 0;
-
-		//encontrando o maior e o menor
-		for (int i=leftIndex; i<=rightIndex; i++) {	
-			if (array[i] > maior) {
-				maior = array[i];
+			//encontrando o maior e o menor
+			for (int i=leftIndex; i<=rightIndex; i++) {	
+				if (array[i] > maior) {
+					maior = array[i];
+				}
+				if (array[i] < menor) {
+					menor = array[i];
+				}
 			}
-			if (array[i] < menor) {
-				menor = array[i];
+		
+			//registrando frequencia dos elementos do array;
+			int[] arrayContagem = new int [(maior - menor) + 1];
+		
+			for (int i2=leftIndex; i2<=rightIndex; i2++){
+				arrayContagem[array[i2]] += 1;
 			}
-		}
-		
-		//registrando frequencia dos elementos do array;
-		int[] arrayContagem = new int [(maior - menor) + 1];
-		
-		for (int i2=leftIndex; i2<=rightIndex; i2++){
-			arrayContagem[array[i2]] += 1;
-		}
 
-		//calculando a soma cumulativa do array auxiliar
-		for (int j=1; j<arrayContagem.length; j++) {
+			//calculando a soma cumulativa do array auxiliar
+			for (int j=1; j<arrayContagem.length; j++) {
 			arrayContagem[j] = arrayContagem[j] + arrayContagem[j - 1];
-		}
+			}
 
-		//ordenando o array
-		int[] arrayOrdenado = new int [array.length];
+			//ordenando o array
+			int[] arrayOrdenado = new int [array.length];
 
-		for (int o=rightIndex; o>=leftIndex; o--) {
-			arrayOrdenado[arrayContagem[array[o]] -1] = array[o];
-			arrayContagem[array[o]] -= 1;
-		}
+			for (int o=rightIndex; o>=leftIndex; o--) {
+				arrayOrdenado[arrayContagem[array[o]] -1] = array[o];
+				arrayContagem[array[o]] -= 1;
+			}
 
-		//insere o array ordenado para nas posições do array inicial
-		for (int i3=leftIndex; i3<=rightIndex; i3++) {
-			array[i3] = arrayOrdenado[i3];
+			//insere o array ordenado para nas posições do array inicial
+			for (int i3=leftIndex; i3<=rightIndex; i3++) {
+				array[i3] = arrayOrdenado[i3];
+			}
 		}
 	}
 
--- R03-02/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-02/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -14,53 +14,51 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
 
 		//tratando casos de teste
-		if (array.length == 0) {
-			return;
-		}
+		if (array.length > 0) {
+			
+			//variáveis
+			int maior = 0;
+			int menor = 0;
 
-
-		//variáveis
-		int maior = 0;
-		int menor = 0;
-
-		//encontrando o maior e o menor
-		for (int i=leftIndex; i<=rightIndex; i++) {	
-			if (array[i] > maior) {
-				maior = array[i];
+			//encontrando o maior e o menor
+			for (int i=leftIndex; i<=rightIndex; i++) {	
+				if (array[i] > maior) {
+					maior = array[i];
+				}
+				if (array[i] < menor) {
+					menor = array[i];
+				}
 			}
-			if (array[i] < menor) {
-				menor = array[i];
+
+			//tratando os numeros negativos
+			if (menor < 0) {
+				menor = Math.abs(menor);
 			}
-		}
+			
+			//registrando frequencia dos elementos do array;
+			int[] arrayContagem = new int [(maior + menor) + 1];
+			
+			for (int i2=leftIndex; i2<=rightIndex; i2++){
+				arrayContagem[array[i2] + menor] += 1;
+			}
 
-		//tratando os numeros negativos
-		if (menor < 0) {
-			menor = Math.abs(menor);
-		}
-		
-		//registrando frequencia dos elementos do array;
-		int[] arrayContagem = new int [(maior + menor) + 1];
-		
-		for (int i2=leftIndex; i2<=rightIndex; i2++){
-			arrayContagem[array[i2] + menor] += 1;
-		}
+			//calculando a soma cumulativa do array auxiliar
+			for (int j=1; j<arrayContagem.length; j++) {
+				arrayContagem[j] = arrayContagem[j] + arrayContagem[j - 1];
+			}
 
-		//calculando a soma cumulativa do array auxiliar
-		for (int j=1; j<arrayContagem.length; j++) {
-			arrayContagem[j] = arrayContagem[j] + arrayContagem[j - 1];
-		}
+			//ordenando o array
+			int[] arrayOrdenado = new int [array.length];
 
-		//ordenando o array
-		int[] arrayOrdenado = new int [array.length];
+			for (int o=rightIndex; o>=leftIndex; o--) {
+				arrayOrdenado[arrayContagem[array[o] + menor] -1] = array[o];
+				arrayContagem[array[o] + menor] -= 1;
+			}
 
-		for (int o=rightIndex; o>=leftIndex; o--) {
-			arrayOrdenado[arrayContagem[array[o] + menor] -1] = array[o];
-			arrayContagem[array[o] + menor] -= 1;
-		}
-
-		//insere o array ordenado para nas posições do array inicial
-		for (int i3=leftIndex; i3<=rightIndex; i3++) {
-			array[i3] = arrayOrdenado[i3];
+			//insere o array ordenado para nas posições do array inicial
+			for (int i3=leftIndex; i3<=rightIndex; i3++) {
+				array[i3] = arrayOrdenado[i3];
+			}
 		}
 	}
 
